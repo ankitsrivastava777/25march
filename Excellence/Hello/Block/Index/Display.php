a4d9d85ecb2906c536613c3ea13062c4425ebe69
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-03-27 17:59:38 +0530

    Pagination implimentaion in customer log table.

--- Excellence/Hello/Block/Index/Display.php
+++ Excellence/Hello/Block/Index/Display.php
@@ -15,10 +15,39 @@ class Display extends \Magento\Framework\View\Element\Template
         $this->testFactory = $testFactory;
 	}
 
-	public function sayHello()
+	public function getUserData()
 	{
 	    $test = $this->testFactory->create();
-        $data = $test->getCollection();
-        return $data->getData();
-	}
+        $collection = $test->getCollection();
+        $page = ($this->getRequest()->getParam('p')) ? $this->getRequest()->getParam('p') : 1;
+        //get values of current limit. if not the param value then it will set to 1
+        $pageSize = ($this->getRequest()->getParam('limit')) ? $this->getRequest()->getParam('limit') : 5;
+        $collection = $this->testFactory->create();
+        $collection = $collection->getCollection();
+        $collection->setPageSize($pageSize);
+        $collection->setCurPage($page);
+        return $collection;    
+    }
+    public function _prepareLayout()
+    {
+        parent::_prepareLayout(); 
+        $this->pageConfig->getTitle()->set(__('Customer Log Records'));
+        
+        if ($this->getUserData())
+        {
+            $pager = $this->getLayout()->createBlock(
+                'Magento\Theme\Block\Html\Pager',
+                'fme.news.pager'
+            )->setAvailableLimit(array(5 => 5   , 10 => 10, 15 => 15))->setShowPerPage(true)->setCollection(
+                $this->getUserData()
+            );
+            $this->setChild('pager', $pager);
+            $this->getUserData()->load();   
+        } 
+        return $this;   
+    }
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
+    }
 }
